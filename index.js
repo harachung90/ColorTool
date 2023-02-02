22c54a3efb0a3e38818fdd87ad8118f0846716c7
Hara Chung <harachung90@gmail.com>
2023-02-02 19:15:37 +1100

    Third commit. Added functions to convert Hex to RGB and RGB to Hex

--- index.js
+++ index.js
@@ -16,3 +16,33 @@ hexInputEl.addEventListener('keyup', (event) => {
     const strippedHex = hex.replace('#', '')
     inputColorEl.style.backgroundColor = "#" + hex;
 })
+
+const convertHexToRGB = (hex) => {
+    if(!isValidHex(hex)) return null;
+
+    let strippedHex = hex.replace('#', '');
+
+    if(strippedHex.length === 3) {
+        strippedHex = strippedHex[0] + strippedHex[0]
+        + strippedHex[1] + strippedHex[1]
+        + strippedHex[2] + strippedHex[2];
+    }
+
+    const r = parseInt(strippedHex.substring(0, 2), 16);
+    const g = parseInt(strippedHex.substring(2, 4), 16);
+    const b = parseInt(strippedHex.substring(4, 6), 16);
+
+    return {r, g, b}
+}
+
+const convertRGBToHex = (r, g, b) => {
+    const firstPair = ("0" + r.toString(16)).slice(-2);
+    const secondPair = ("0" + g.toString(16)).slice(-2);
+    const thirdPair = ("0" + b.toString(16)).slice(-2);
+
+    const hex = "#" + firstPair + secondPair + thirdPair;
+    return hex;
+
+}
+convertHexToRGB('333');
+console.log(convertRGBToHex(0,10,255))
